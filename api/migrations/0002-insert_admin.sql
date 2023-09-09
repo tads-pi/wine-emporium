dda2422280c1f1b43d153f7d57f11c6e2a3936a2
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-09-09 19:54:50 -0300

    added deploy to develop

--- api/migrations/0002-insert_admin.sql
+++ api/migrations/0002-insert_admin.sql
@@ -7,6 +7,6 @@ VALUES
         "012.345.678-90",
         "email@example.com",
         "$2a$10$wzRsIlTgaUrRFGkRLLeA.O0oUhTD5774geI/adPfC8y0SL0wjUimW",
-        "2021-01-01 00:00:00",
-        "2021-01-01 00:00:00"
+        "2023-01-01 00:00:00",
+        "2023-01-01 00:00:00"
     );
