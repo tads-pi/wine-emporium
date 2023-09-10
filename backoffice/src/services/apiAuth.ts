e63e70c1740ff1c5adc3dd74a96ecdaa821ba353
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-09-09 23:03:48 -0300

    kinda fixed api uri

--- backoffice/src/services/apiAuth.ts
+++ backoffice/src/services/apiAuth.ts
@@ -1,7 +1,15 @@
 import axios from 'axios'
 
+function getUri() {
+    switch (import.meta.env.MODE) {
+        case "production": return "https://api.wineemporium.shop/v1"
+        case "development": return "https://api.dev.wineemporium.shop/v1"
+        case "local": return "http://localhost:8080/v1"
+    }
+}
+
 const apiAuth = axios.create({
-    baseURL: import.meta.env.API_URL,
+    baseURL: getUri(),
 })
 
 export default apiAuth
