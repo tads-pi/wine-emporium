4c2b6b6267fc97100969eacdb38579f89a123623
thomazinig <guilherme_thomazini245@hotmail.com>
2023-09-27 17:39:05 -0300

    adicionado pagina detalhe produto

--- store/src/App.jsx
+++ store/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./home"
 import { NavBar } from "./components/NavBar"
 import { FormCreateUser } from "./components/FormCreateUser"
+import { VerMais } from "./components/verMais"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mercado" element={<NavBar />} />
+        <Route path="/mercado/:id" element={<VerMais/>}/>
         <Route path="/criar-cadastro" element={<FormCreateUser />} />
       </Routes>
     </BrowserRouter>
--- store/src/lib/axios.ts
+++ store/src/lib/axios.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-
 export const api = axios.create({
   baseURL: 'https://api.wineemporium.shop',
 });
