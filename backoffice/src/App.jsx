932f57bb1b07a0baca41717e51a0799755258f5f
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-10-07 02:09:38 -0300

    moved backoffice login path

--- backoffice/src/App.jsx
+++ backoffice/src/App.jsx
@@ -37,7 +37,7 @@ export default function App() {
                 <Routes>
                     <Route path="*" element={<PageNotFound />} />
                     <Route
-                        path="/login"
+                        path="/"
                         element={<Login />}
                     />
                     <Route
