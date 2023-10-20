ae78d82c4850db264228e3d8796d72fdaa8b2b86
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-10-20 13:26:05 -0300

    fix live preview

--- backoffice/src/components/LivePreview/LivePreviewWE.jsx
+++ backoffice/src/components/LivePreview/LivePreviewWE.jsx
@@ -1,7 +1,7 @@
 export default function LivePreviewWE({ component, containerClassName }) {
     return (
         <div>
-            <h1>Live Preview</h1>
+            <h1>Visualização ao vivo</h1>
             <div className={containerClassName}>
                 {component}
             </div>
