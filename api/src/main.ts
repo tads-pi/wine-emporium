4397ef58ec8bb2650cfd9ec3b38fa4f5b6508503
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-10-28 19:14:04 -0300

    clean up

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    // essa flag define que as dtos só podem ter os campos informados no tipo
+    // evitando sql injection e etc!
+    whitelist: true,
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Wine Emporium')
+    .setDescription('Wine Emporium API')
+    .setVersion('0.2')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,24 +1,13 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+describe('teste end-to-end', () => {
+  //setup
+  beforeAll(async () => {
+    it.todo('TODO')
+  })
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  //cleanup
+  afterAll(async () => {
+    it.todo('TODO')
+  })
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
-
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
-});
+  //testes
+})
